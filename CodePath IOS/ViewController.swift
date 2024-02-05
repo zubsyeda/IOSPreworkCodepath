6486b9ad8fd623169cc9f9d2e810d8e762a3d940
zubsyeda <125157533+zubsyeda@users.noreply.github.com>
2024-02-04 21:48:06 -0600

    Added Prework Files to Repository

---
+++ CodePath IOS/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  CodePath IOS
+//
+//  Created by Zubiya Syeda on 2/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+    func changeColor() -> UIColor{
+
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
+}
+
